fabe31ec7090d01b85f956e083f02741918a893c
mitchay <mitchvanloon@yahoo.com>
2021-03-31 23:18:56 +0000

    ran provided

--- Midterm/q1.cpp
+++ Midterm/q1.cpp
@@ -4,20 +4,11 @@ using namespace std;
 
 int main () 
 {
-   
-//variables to hold the values of each type of ticket
-float classa = 15.00 , classb = 12, classc = 9, amta, amtb, amtc, total;
-cout << "please enter the amount of tickets for class A ";
-cin >> amta;
-cout << "please enter the amount of tickets for class B ";
-cin >> amtb;
-cout << "please enter the amount of tickets for class C ";
-cin >> amtc;
-total = (classa * amta) + (classb * amtb) + (classc * amtc);
-cout << "the total is $ " <<  std::fixed << std::setprecision(2) << total << "\n"; 
-return 0;
-cout << total;
-//displaying the results and rounding them
-
-return 0;
+    const  int N=5, M=5;
+    int     i, j;
+    for(i=0;i<N;i++) {
+        for(j=i; j<M-i; j++)
+            cout << " (" << i << "," << j << ") " ;
+        cout << endl;
+    }
 }
