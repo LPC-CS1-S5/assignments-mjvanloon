bacccc476280fc981f844ca4c6bcc0877705bb7c
mitchay <mitchvanloon@yahoo.com>
2021-04-01 01:09:48 +0000

    q4

---
+++ Midterm/q4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int getrnd(void);
+return rand() % 100 + 1;
+
+int main()
+{
+int a = 1, b = 2, c = 3;
+int result1, result2, result3;
+result1 = getrnd
+result2 getrnd(b)
+result3 getrnd(a,b)
+}
