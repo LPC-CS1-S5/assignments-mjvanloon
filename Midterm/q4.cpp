ba48bf1e335dce637e63f995457d6629a44be58f
mitchay <mitchvanloon@yahoo.com>
2021-04-01 01:18:48 +0000

    q4

--- Midterm/q4.cpp
+++ Midterm/q4.cpp
@@ -5,13 +5,22 @@
 using namespace std;
 
 int getrnd(void);
+{
 return rand() % 100 + 1;
-
+}
+int getrnd(int a);
+{
+return rand() % a + 1;
+}
+int getrnd(int a, int b);
+{
+return rand() % 1 + b + a;
+}
 int main()
 {
 int a = 1, b = 2, c = 3;
 int result1, result2, result3;
-result1 = getrnd
-result2 getrnd(b)
-result3 getrnd(a,b)
+result1 = getrnd;
+result2 getrnd(b);
+result3 getrnd(a,b);
 }
