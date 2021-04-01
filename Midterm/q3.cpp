93c75f65c50101a8074505679f64b1f03c5ceb03
mitchay <mitchvanloon@yahoo.com>
2021-04-01 00:43:55 +0000

    q3 2

--- Midterm/q3.cpp
+++ Midterm/q3.cpp
@@ -7,28 +7,24 @@ int getrnd(void);
 {
 return rand() % 50 + 1;
 }
-int isgreater(int n);
+int isgreater(int a, int b);
+{
+if (a > b)
+return 1
+else
+    return 0
+}
 
 int main()
 {
     ofstream myfile;
-    int i, num, score1, score2;
-    string name;
-    cout << "enter the number of students ";
-    cin >> i;
+    int i, num, rand1, randn; 
+    rand1 = getrnd;
+    randn= getrnd;
     myfile.open("numbers.txt");
-    for (int j = 1; j <= i; j++)
-    {
-        cout << "enter the student name  ";
-        cin >> name;
-        cout << "enter the first score ";
-        cin >> score1;
-        cout << "enter the second score  ";
-        cin >> score2;
-         myfile << name << " " << score1 << " " << score2 << endl;
-        
-    }
-
+    for (i = 1; i == 5, i++)
+    if (rand1 < randn)
+    cout << randn;
     myfile.close();
     return 0;
 }
