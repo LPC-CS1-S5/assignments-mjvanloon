528c60ca09c9f5d6525d7f64ffac9ae6d6b76ca6
mitchay <mitchvanloon@yahoo.com>
2021-04-01 01:03:12 +0000

    q5 pt1

---
+++ Midterm/q5.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int getinput;
+{
+int a
+cin >> a 
+return a
+}
+issame (int a, int b)
+{
+if (a==b)
+break
+}
+int division ( int a, int b)
+{
+if (a > b)
+    result = (a/b
+    else
+    result = (a/b);
+}
+int filewrite
+
+int main()
+{
+    ofstream myfile;
+    int n1, n2, result;
+    n1 = getinput;
+    n2 = getinput;
+    issame (n1, n2);
+    myfile.open("question5.txt");
+    if (n1 > n2)
+    result = (n1/n2)
+    else
+    result = (n2/n1);
+else
+if (n1 == n2)
+exit;
+
+    myfile.close();
+    return 0;
+}
