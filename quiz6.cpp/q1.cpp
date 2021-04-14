1b41d814a2b21114d27da6e49710d8c501b09f05
mitchay <>
2021-04-14 02:11:25 +0000

    q1 2

--- quiz6.cpp/q1.cpp
+++ quiz6.cpp/q1.cpp
@@ -5,15 +5,18 @@
 using namespace std;
 
 void swaptwovalues(int n1, int n2)
-
+{
+return n2, n1;
+}
 void swaptwovalues (float f1, float f2)
-
+{
+return f2, f1
+}
 int main()
 {
-    int a, b;
-    float c, d;
+    float a, b;
 
 cout << "enter the two values \n";
-cin >> a >> b;\
-
+cin >> a >> b;
+if 
 }
