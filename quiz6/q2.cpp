dc2f3143a3efe57179247fbf25099a3c71d32685
mjvanloon <70118394+mjvanloon@users.noreply.github.com>
2021-04-13 20:06:37 -0700

    Update q2.cpp

--- quiz6/q2.cpp
+++ quiz6/q2.cpp
@@ -7,11 +7,23 @@ using namespace std;
 void getrnd (int n1, int n2, int n3)
 {
     return rand() % 100 + 1;
-    return rand() % 100 + 1;
-    return rand() % 100 + 1;
+    
 }
 
 int findmin(int n1, int n2, int n3)\
 {
-    
+std ::min(n1,n2,n3)
+}
+void printresult(int a)
+{
+cout << a;
+}
+
+int main()
+{
+int min;
+int a, b, c;
+getrnd (a, b,c);
+findmin (a,b,c);
+printresult
 }
