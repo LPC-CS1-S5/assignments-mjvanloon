0e331152548fa84f46d3db2fa2b0a834eb7668ec
mitchay <>
2021-04-14 02:00:53 +0000

    q1

--- quiz6/q1.cpp
+++ quiz6/q1.cpp
@@ -1,1 +1,19 @@
-a
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+void swaptwovalues(int n1, int n2)
+
+void swaptwovalues (float f1, float f2)
+
+int main()
+{
+    int a, b;
+    float c, d;
+
+cout << "enter the two values \n";
+cin >> a >> b;\
+
+}
