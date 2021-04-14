632aeea4d816581fdc223c0d392167b3a25cfd64
mitchay <>
2021-04-14 02:54:57 +0000

    q2 pt 1

---
+++ quiz6.cpp/q2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+void getrnd (int n1, int n2, int n3)
+{
+    return rand() % 100 + 1;
+    return rand() % 100 + 1;
+    return rand() % 100 + 1;
+}
+
+int findmin(int n1, int n2, int n3)\
+{
+    
+}
