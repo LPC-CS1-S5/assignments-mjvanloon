0123c593ef12dbfbc05dbff6abf7837d9d505598
mjvanloon <70118394+mjvanloon@users.noreply.github.com>
2021-04-13 20:16:08 -0700

    Update q2.cpp

--- quiz6/q2.cpp
+++ quiz6/q2.cpp
@@ -4,19 +4,23 @@
 #include <ctime>
 using namespace std;
 
-void getrnd (int n1, int n2, int n3)
+void getrnd (int &a, int &b, int &c)
 {
-    return rand() % 100 + 1;
+    a = rand() % 100 + 1;
+    b = rand() % 100 + 1;
+    c = rand() % 100 + 1;
     
 }
 
-int findmin(int n1, int n2, int n3)\
+int findmin(int n1, int n2, int n3)
 {
-std ::min(n1,n2,n3)
+    int a;
+a = std::min(n1,n2,n3);
+return a;
 }
-void printresult(int a)
+void printresult(int n1, int n2, int n3, int min)
 {
-cout << a;
+cout << n1 << " , " << n2 << " , " << n3 << " , " << "the minimum is " < min;
 }
 
 int main()
@@ -24,6 +28,6 @@ int main()
 int min;
 int a, b, c;
 getrnd (a, b,c);
-findmin (a,b,c);
-printresult
+min = findmin (a,b,c);
+printresult ( a, b, c, min)
 }
