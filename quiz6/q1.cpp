df273f0b7ade1ee4b71f6a9098a1e961bd6b7d21
mjvanloon <70118394+mjvanloon@users.noreply.github.com>
2021-04-13 19:20:47 -0700

    Update q1.cpp

--- quiz6/q1.cpp
+++ quiz6/q1.cpp
@@ -5,15 +5,18 @@
 using namespace std;
 
 void swaptwovalues(int n1, int n2)
-
+{
+cout << n2 << " , " << n1;
+}
 void swaptwovalues (float f1, float f2)
-
+{
+cout << f2 << " , " << f1;
+}
 int main()
 {
-    int a, b;
-    float c, d;
-
-cout << "enter the two values \n";
-cin >> a >> b;\
-
-}
+    float a = 1.5;
+    float b = 2.4;
+    int c = 1;
+    int d = 2;
+swaptwovalues (a, b);
+swaptwovalues (a,b)
