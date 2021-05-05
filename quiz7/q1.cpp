4e7b9c0f6424f6c500ae74b55640e6fd935d48bf
mitchay <>
2021-05-05 05:04:34 +0000

    q1 final

--- quiz7/q1.cpp
+++ quiz7/q1.cpp
@@ -4,9 +4,27 @@
 using namespace std;
 
 const int N = 10;
-
+int i=0;
 int main() {
-    int number[N] = {10, 15, 0, -25, 19, 20, 25, 30, 45, 20};
+    int numbers[N] = {10, 15, 0, -25, 19, 20, 25, 30, 45, 20};
 
+if (abs(numbers[i] - numbers[i + 1]) < 10)
+{
+cout << numbers[i];
+cout << " -the difference between adjacent elements is less than 10\n";
+}
+
+for (i=1; i < 9; i++)
+{
+if ((abs(numbers[i-1] - numbers[i]) < 10) && (abs(numbers[i+1] - numbers[i]) < 10))
+	cout << numbers[i] <<  " -the difference between adjacent elements is less than 10\n";
+
+}
+
+if (abs(numbers[i-1] - numbers[i]) < 10)
+{
+cout << numbers[i];
+cout << " -the difference between adjacent elements is less than 10\n";
+}
 }
 
