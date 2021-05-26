a389948b57532b2a5b78f08cd2d270cfd113eda4
mitchay <>
2021-05-26 17:02:22 +0000

    Question 3 better version. First commit was rough draft.

--- final/q3.cpp
+++ final/q3.cpp
@@ -1,44 +1,33 @@
 #include <bits/stdc++.h>
 #include <iostream>
-bool isSubset(int arr1[], int arr2[],
-              int m, int n)
+bool checksubset(int array1[], int array2[],
+              int x, int y)
 {
     int i = 0;
     int j = 0;
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < m; j++) {
-            if (arr2[i] == arr1[j])
+    for (i = 0; i < y; i++) {
+        for (j = 0; j < x; j++) {
+            if (array2[i] == array1[j])
                 break;
         }
  
-       
-        if (j == m)
+        if (j == x)
             return 0;
+
     }
- 
-   
     return 1;
 }
- 
-
 int main()
 {
+
 const int SIZE1=5;
 const int SIZE2=10;
 int numbers1[SIZE1] = {2, 3, 5, 9, 10};
 int numbers2[SIZE2] = {1, 2, 3, 5, 9, 10, 11, 13, 15, 17};
 
-    int arr1[] = { 11, 1, 13, 21};
-    int arr2[] = { 11, 1 };
- 
-    int m = sizeof(arr1) / sizeof(arr1[0]);
-    int n = sizeof(arr2) / sizeof(arr2[0]);
- 
-    if (isSubset(arr1, arr2, m, n))
-        printf("arr2[] is a subset of arr1[] ");
+    if (checksubset(numbers2, numbers1, SIZE2, SIZE1))
+        printf("numbers1 is a subset of numbers2 ");
     else
-        printf("arr2[] is not a subset of arr1[]");
- 
-    getchar();
+        printf("numbers1 is not a subset of numbers2");
     return 0;
 }
